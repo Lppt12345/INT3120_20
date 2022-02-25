02115b86080488d9ef93559160062d554f11d7c7
Lppt12345 <aibiettoi19@gmail.com>
2022-02-25 09:22:54 +0700

    Tạo project

---
+++ lesson3/android/app/src/main/kotlin/com/ha/helloapp/lesson3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ha.helloapp.lesson3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lesson6/android/app/src/main/kotlin/com/lesson6/lesson6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lesson6.lesson6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
