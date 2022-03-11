4d9c6a5bcc534ee8e58f3ce6dd0836f4fa41a9e6
Lppt12345 <aibiettoi19@gmail.com>
2022-03-11 12:24:08 +0700

    lesson 22

--- lesson22/lib/main.dart
+++ lesson22/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lesson 22 - 19021262',
       theme: ThemeData(
         // This is the theme of your application.
         //
